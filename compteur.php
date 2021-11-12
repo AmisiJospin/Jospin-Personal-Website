b86d28cc428fa77e4879f42bf81509309d768d65
Amisi Jospin Hassan <40860198+AmisiJospin@users.noreply.github.com>
2021-11-12 08:55:34 +0200

    I add a comment on the code

--- compteur.php
+++ compteur.php
@@ -1,10 +1,12 @@
 <script src="bootstrap/js/jquery-1.7.2.js" type="text/javascript" charset="utf-8"></script>
 
+<!-- This code is counting the number of people who visited the website -->
 <script type="text/javascript" id="servicewb_125726">
-(function() {
-var cv = document.createElement("script"); cv.type = "text/javascript"; cv.async = true;
-cv.src = "http://www.compteur-visite.com/service.php?v=1.1&id=125726&k=bf554d0f39af4163fa34887639aaea17&c="+document.cookie;
-var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(cv, s);
-})();
+  (function() 
+   {
+      var cv = document.createElement("script"); cv.type = "text/javascript"; cv.async = true;
+      cv.src = "http://www.compteur-visite.com/service.php?v=1.1&id=125726&k=bf554d0f39af4163fa34887639aaea17&c="+document.cookie;
+      var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(cv, s);
+  })();
 </script>
 <noscript><a href="http://www.compteur-visite.com/">Services gratuits pour webmasters</a></noscript>
